b8c4d548edede07b7131cb119cdd967f198f99b5
Richard Brown <fluke_fan@hotmail.com>
2018-01-24 09:15:25 +0000

    Reformatted to fit into smaller width

--- Test/Program.cs
+++ Test/Program.cs
@@ -18,13 +18,13 @@ namespace Test
             Console.WriteLine($"Timing Results");
             Console.WriteLine($"==============\n\n");
             Console.WriteLine($"dotnet.exe: {timings.DotnetVersion}\n");
-            Console.WriteLine("Description                     Build          Incremental     Incremental     ");
-            Console.WriteLine("                                               (no changes)    (with change)   ");
-            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine("Description                Build       Incremental  Incremental  ");
+            Console.WriteLine("                                       (no changes) (with change)");
+            Console.WriteLine("-----------------------------------------------------------------");
 
             foreach (var timing in timings.Frameworks)
             {
-                Console.WriteLine($"{timing.Description.PadRight(31)} {Seconds(timing.Build).PadRight(15)} {Seconds(timing.IncrementalBuildWithoutChange).PadRight(15)} {Seconds(timing.IncrementalBuildWithChange).PadRight(15)}");
+                Console.WriteLine($"{timing.Description.PadRight(25)} {Seconds(timing.Build).PadRight(12)} {Seconds(timing.IncrementalBuildWithoutChange).PadRight(12)} {Seconds(timing.IncrementalBuildWithChange).PadRight(12)}");
             }
 
             Console.WriteLine("\nMultiplier\n");
@@ -47,7 +47,7 @@ namespace Test
                 var incrementalWithoutChangeMultiplier = incrementalWithoutChangeTicks / (double)baselineIncrementalWithoutChange;
                 var incrementalWithChangeMultiplier = incrementalWithChangeTicks / (double)baselineIncrementalWithChange;
 
-                Console.WriteLine($"{timing.Description.PadRight(31)} {Multiplier(buildMultiplier).PadRight(15)} {Multiplier(incrementalWithoutChangeMultiplier).PadRight(15)} {Multiplier(incrementalWithChangeMultiplier).PadRight(15)}");
+                Console.WriteLine($"{timing.Description.PadRight(25)} {Multiplier(buildMultiplier).PadRight(12)} {Multiplier(incrementalWithoutChangeMultiplier).PadRight(12)} {Multiplier(incrementalWithChangeMultiplier).PadRight(12)}");
             }
         }
 
